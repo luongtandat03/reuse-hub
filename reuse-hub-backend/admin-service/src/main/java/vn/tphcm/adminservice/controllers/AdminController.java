c7b07b4b8318b7ade11ca3d6eb194f39afe1ba7a
LTDat5 <88955236+LTDat03@users.noreply.github.com>
2026-05-07 16:50:37 +0700

    test ci

--- reuse-hub-backend/admin-service/src/main/java/vn/tphcm/adminservice/controllers/AdminController.java
+++ reuse-hub-backend/admin-service/src/main/java/vn/tphcm/adminservice/controllers/AdminController.java
@@ -32,9 +32,6 @@ public class AdminController {
                                                          @RequestParam(defaultValue = "createdAt") String sortBy,
                                                          @RequestParam(defaultValue = "desc") String sortDirection){
         
-        log.info("AdminController - getAllUsers: Received request to fetch all transactions with pageNo={}, pageSize={}, sortBy={}, sortDirection={}",
-                pageNo, pageSize, sortBy, sortDirection);
-
         return adminService.getAllUsers(pageNo, pageSize, sortBy, sortDirection);
     }
 
